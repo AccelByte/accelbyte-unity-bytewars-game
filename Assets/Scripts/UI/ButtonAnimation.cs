1f070240721a638f2b191d79849778870d18479d
Nathania Saphira <nathania@accelbyte.net>
2023-09-25 03:57:24 +0700

    chore: add party button in Social and on disable interactable color change implementation

--- Assets/Scripts/UI/ButtonAnimation.cs
+++ Assets/Scripts/UI/ButtonAnimation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
@@ -16,6 +17,11 @@ public class ButtonAnimation : MonoBehaviour, IPointerEnterHandler, IDeselectHan
     void Start()
     {
       button.onClick.AddListener(OnClickAnimation);
+    }
+
+    private void Update()
+    {
+        text.color = button.IsInteractable() ? Color.white : Color.gray;
     }
 
     void OnClickAnimation()
