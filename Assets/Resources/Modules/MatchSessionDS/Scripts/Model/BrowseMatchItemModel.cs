a6a0c7fba008df8a24cd1ac08f072b87313a99a2
Kemal Maulidiasani <kemal.maulidiasani@accelbyte.net>
2024-07-22 07:01:28 +0000

    Merged in bugfix/DEVED-1543-unity-player-name-is-not-displayed-in-browse-menu (pull request #198)
    Approved-by: Nauval Muhammad Firdaus
    Approved-by: Hary Mulyadi

--- Assets/Resources/Modules/MatchSessionDS/Scripts/Model/BrowseMatchItemModel.cs
+++ Assets/Resources/Modules/MatchSessionDS/Scripts/Model/BrowseMatchItemModel.cs
@@ -13,7 +13,6 @@ public class BrowseMatchItemModel
     public int MaxPlayerCount { get; private set; }
     public int CurrentPlayerCount { get; private set; }
     public Action<BrowseMatchItemModel> OnDataUpdated;
-    private const string ClassName = "[BrowseMatchItemModel]";
     private const string DefaultWarName = "player's match";
 
     public BrowseMatchItemModel(SessionV2GameSession gameSession, int index = -1)
@@ -43,21 +42,20 @@ public class BrowseMatchItemModel
         SetPlayerCount(updatedModel);
         OnDataUpdated?.Invoke(this);
     }
+
     private void OnPublicUserDataRetrieved(Result<PublicUserData> result)
     {
         if (result.IsError)
         {
-            Debug.LogWarning($"{ClassName} OnPublicUserDataRetrieved error: {result.Error.Message}");
+            BytewarsLogger.LogWarning($"Error: {result.Error.Message}");
         }
         else
         {
-            var publicUserData = result.Value;
-            var creatorName = DefaultWarName;
-            if (!String.IsNullOrEmpty(publicUserData.displayName))
-            {
-                creatorName = publicUserData.displayName + "'s war";
-            }
-            MatchCreatorName = creatorName;
+            PublicUserData publicUserData = result.Value;
+            string truncatedUserId = publicUserData.userId[..5];
+            string displayName = string.IsNullOrEmpty(publicUserData.displayName) ?
+                $"Player-{truncatedUserId}" : publicUserData.displayName;
+            MatchCreatorName = $"{displayName}'s match";
             MatchCreatorAvatarURL = publicUserData.avatarUrl;
             OnDataUpdated?.Invoke(this);
         }
@@ -66,9 +64,9 @@ public class BrowseMatchItemModel
     private int GetJoinedPlayerCount(SessionV2MemberData[] members)
     {
         int joinedMemberCount = 0;
-        for (var i = 0; i < members.Length; i++)
+        for (int i = 0; i < members.Length; i++)
         {
-            var member = members[i];
+            SessionV2MemberData member = members[i];
             if (member.status == SessionV2MemberStatus.JOINED)
             {
                 joinedMemberCount++;
@@ -79,7 +77,7 @@ public class BrowseMatchItemModel
 
     private void SetMatchTypeAndServerType(SessionV2GameSession gameSession)
     {
-        var gameSessionName = gameSession.configuration.name;
+        string gameSessionName = gameSession.configuration.name;
         if (gameSessionName.Equals(GameSessionConfig.UnitySessionEliminationDS))
         {
             GameMode = InGameMode.CreateMatchEliminationGameMode;
